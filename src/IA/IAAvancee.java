51be5a36cc44cdb0556cfadbc3fc1f9f15bb3778
landry <landry.rolland@gmx.fr>
2022-05-02 11:13:56 +0200

    ajout classe IAAvance

---
+++ src/IA/IAAvancee.java
@@ -0,0 +1,21 @@
+package IA;
+
+import General.Grille;
+import java.awt.*;
+
+public class IAAvancee {
+
+    Grille g;
+    int lignes;
+    int colonnes;
+    int cases_ok;
+
+
+    public IAAvancee(Grille grille){
+        g = grille;
+        lignes = g.lignes();
+        colonnes = g.colonnes();
+    }
+
+    //TODO
+}
